b483c9b82275ac7e017778b9dc301daf47138543
unknown <jaspal.singh@unthinkable.co>
2024-06-24 08:22:15 +0530

    latest-v

--- Automationmavenjenkins/src/main/java/com/automationmavenjenkins/Automationmavenjenkins/LaunchTest.java
+++ Automationmavenjenkins/src/main/java/com/automationmavenjenkins/Automationmavenjenkins/LaunchTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+
 public class LaunchTest {
 
 	static String url;
